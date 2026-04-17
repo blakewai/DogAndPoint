9de90ffc6c539f40b35d205b2cba5f08fa7f1161
D3muga <blakewai56@gmail.com>
2026-04-17 14:17:53 +0300

    Edit

---
+++ DogAndPaint/Pages/FrameMainPanel/Address.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DogAndPower.Pages.FrameMainPanel
+{
+    /// <summary>
+    /// Логика взаимодействия для Address.xaml
+    /// </summary>
+    public partial class Address : Page
+    {
+        public Address()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- DogAndPaint/Pages/MainPages/MainPanel.xaml.cs
+++ DogAndPaint/Pages/MainPages/MainPanel.xaml.cs
@@ -23,27 +23,36 @@ namespace DogAndPaint.Pages.MainPages
         public MainPanel()
         {
             InitializeComponent();
+            PanelLogic();
+        }
+
+        private void PanelLogic()
+        {
             Logic.TitleWindowEdit.Title("Главная страница");
+            FramesMainPanel.Navigate(new Uri("Pages/FrameMainPanel/Client/MainPagePanel.xaml", UriKind.RelativeOrAbsolute));
         }
 
         private void MainPanelBT_Click(object sender, RoutedEventArgs e)
         {
-
+            PanelLogic();
         }
 
         private void MenuTB_Click(object sender, RoutedEventArgs e)
         {
-
+            Logic.TitleWindowEdit.Title("Меню");
+            FramesMainPanel.Navigate(new Uri("Pages/FrameMainPanel/Client/Menu.xaml", UriKind.RelativeOrAbsolute));
         }
 
         private void ProfileTB_Click(object sender, RoutedEventArgs e)
         {
-
+            Logic.TitleWindowEdit.Title("Профиль");
+            FramesMainPanel.Navigate(new Uri("Pages/FrameMainPanel/Profile.xaml", UriKind.RelativeOrAbsolute));
         }
 
         private void Address_Click(object sender, RoutedEventArgs e)
         {
-
+            Logic.TitleWindowEdit.Title("Точки");
+            FramesMainPanel.Navigate(new Uri("Pages/FrameMainPanel/Address.xaml", UriKind.RelativeOrAbsolute));
         }
     }
 }
